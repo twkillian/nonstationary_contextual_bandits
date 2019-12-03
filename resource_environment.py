951098a49a8aacaba09f760d8a5d053b9b006fa8
Taylor Killian <twkillian@cs.toronto.edu>
2019-12-02 20:08:02 -0500

    Updated arm selection notes ... need to use a BNN...

--- resource_environment.py
+++ resource_environment.py
@@ -11,6 +11,14 @@ This file contains the classes and utilities defining the CB+Resource Management
 # Provide methods to:
 # 
 #    - Select arms based on context (user preferences and number of items remaining) + Thompson Sampling
+#        - It seems that it might be best to use a BNN for the reward prediction since we can comfortably adjust
+#          for context as well as the various actions. If I use a GP, it might get a little unwieldly since we'll 
+#          need to train a GPLVM for each action and might need significant numbers of users to get the regressions to work
+#        - We can't use BLR since the expected reward should vanish if an item is 'gone' and such an item is chosen. This
+#          is inherently a non-linear relationship in the context.
+#        - The true reward will be something of the form:
+#                  r = \sum_j (val_j*Pr(usrpref_j))*I(amtRemaining_j>0)
+#           where Pr(usrpref_j) is a bernoulli random variable \in {0,1} based on the probabilty of usrpref_j
 #    - Sample from user preferences whether they "accept" the item
 #    - Update arms/resources available
 #    - Update BLR or other context regression algorithm
